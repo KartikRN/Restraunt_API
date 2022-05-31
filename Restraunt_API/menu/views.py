c91bfce89a4bf8abf3702f9dfd9be0b3972f193f
Kartik <nandagaonkartik@gmail.com>
2022-05-31 19:06:27 +0530

    Add to menu and View menu

--- Restraunt_API/menu/views.py
+++ Restraunt_API/menu/views.py
@@ -1,13 +1,21 @@
 import requests
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Menu
 from .serializers import MenuSerializer
 # Create your views here.
 
+@api_view(['GET','POST'])
 def Menu_list(request):
-    menu = Menu.objects.all()
+    if request.method == 'GET':
+        menu = Menu.objects.all()
+        serializer = MenuSerializer(menu, many=True)
+        return JsonResponse({'menu':serializer.data}, safe=False)
+    if request.method == 'POST':
+        serializer = MenuSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
